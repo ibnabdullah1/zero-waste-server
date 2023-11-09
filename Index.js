7ddcc51c717e33105b11f8a2aa9f96e439b0ab12
abdullah01 <arfathossen541@gmail.com>
2023-11-09 11:37:56 +0600

    middleware deleted

--- Index.js
+++ Index.js
@@ -7,13 +7,6 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173"],
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://zero-waste-5fb87.web.app"],
